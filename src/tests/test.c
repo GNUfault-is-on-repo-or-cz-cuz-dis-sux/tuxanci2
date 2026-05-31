b88acd9533bec1e2a95dcccbc02bfedede6a2a7e
Connor Thomson <bluMATRIKZ@gmail.com>
2026-05-31 12:11:21 -0700

    Add tests

---
+++ src/tests/test.c
@@ -0,0 +1,33 @@
+/*
+ * Tuxánci 2 - Successor to Tuxánci
+ * Copyright (C) 2026  Connor Thomson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 201112L
+#   error "Your compiler only supports C99 or lower, this needs C11"
+#endif
+
+#if !defined(__GNUC__) && !defined(__clang__)
+#   error "Unsupported compiler, this needs GCC or clang"
+#endif
+
+#if !defined(_DEFAULT_SOURCE)
+#   error "_DEFAULT_SOURCE is not defined, please define it"
+#endif
+
+int main(void) {
+    return 0;
+}
